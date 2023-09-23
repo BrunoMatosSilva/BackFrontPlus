65017f1a1637d8f9e27a351d149bc2f54c48b033
BrunoMatosSilva <bmsdevfullstack@gmail.com>
2023-09-23 19:42:05 -0300

    MessengerAppBF

---
+++ MessagemAppBackFront/MessagemAppBackFront/Home/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessagemAppBackFront
+//
+//  Created by Bruno Matos Silva on 23/09/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MessagemAppBackFront/MessagemAppBackFront/Home/Screen/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  MessagemAppBackFront
+//
+//  Created by Bruno Matos Silva on 23/09/23.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
